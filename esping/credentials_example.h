c7e497d8cdc827f2b93932cfa33eca125564b641
Kaan Aksoy <kaan.aksoy@kaksoy.com>
2025-06-29 23:34:24 +0300

    Initial Commit

---
+++ esping/credentials_example.h
@@ -0,0 +1,29 @@
+#ifndef CREDENTIALS_H
+#define CREDENTIALS_H
+
+/**************************************************************************/
+/*!
+    @file     credentials_example.h
+    @brief    Header file for credentials
+
+    @details  Contains credentials for the project.
+    @note     This file is not used in the project, it is only an example.
+
+    HOW TO USE:
+    - Create a new file called credentials.h
+    - Copy the contents of this file into credentials.h
+    - Replace the credentials with your own
+*/
+/**************************************************************************/
+
+// WiFi credentials
+#define WIFI_SSID "Your_WiFi_SSID"
+#define WIFI_PASSWORD "Your_WiFi_Password"
+
+// MQTT credentials
+#define MQTT_SERVER "192.168.1.xxx"
+#define MQTT_PORT 1883 // Default port for MQTT
+#define MQTT_USER "your_username"
+#define MQTT_PASSWORD "your_password"
+
+#endif
---
+++ esping/esping.ino
@@ -0,0 +1,97 @@
+#include <WiFi.h>
+#include <PubSubClient.h>
+
+#define LED_PIN         15
+#define MQTT_CLIENT_ID  "esping-01"
+#define STATE_TOPIC     "esping/state"
+#define SLEEP_TOPIC     "esping/prevent-sleep"
+#define WIFI_TIMEOUT    5000
+
+#if __has_include("credentials.h")
+#include "credentials.h"
+#else
+#include "credentials_example.h"
+#endif
+
+WiFiClient wifiClient;
+PubSubClient mqttClient(wifiClient);
+
+bool preventSleep = false;
+unsigned long lastBlink = 0;
+
+void callback(char* topic, byte* payload, unsigned int length) {
+  preventSleep = (length > 0 && payload[0] == '1');
+}
+
+
+void goToSleep() {
+  mqttClient.disconnect();
+  WiFi.disconnect(true);
+  esp_deep_sleep_start();
+}
+
+void setup() {
+  Serial.begin(115200);
+
+  pinMode(LED_PIN, OUTPUT);
+  digitalWrite(LED_PIN, HIGH);
+
+  WiFi.mode(WIFI_STA);
+  WiFi.persistent(false);
+  WiFi.setSleep(false);
+  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+
+  unsigned long t0 = millis();
+  while (WiFi.status() != WL_CONNECTED && millis() - t0 < WIFI_TIMEOUT) {
+    delay(5);
+  }
+
+  if (WiFi.status() != WL_CONNECTED) {
+    goToSleep();
+  }
+
+  mqttClient.setServer(MQTT_SERVER, MQTT_PORT);
+  mqttClient.setKeepAlive(10);
+
+  while (!mqttClient.connected()) {
+    if (mqttClient.connect(MQTT_CLIENT_ID, MQTT_USER, MQTT_PASSWORD)) {
+        delay(25);
+        mqttClient.publish(STATE_TOPIC, "pressed", /*retain=*/true);
+    } 
+    else {
+      delay(50);
+    }
+  }  
+  delay(100);
+  mqttClient.publish(STATE_TOPIC, "released", /*retain=*/true);
+
+  delay(50);
+
+  mqttClient.setCallback(callback);
+  mqttClient.subscribe(SLEEP_TOPIC, 1);
+
+  // Wait briefly to allow incoming messages
+  unsigned long checkTime = millis();
+  while (millis() - checkTime < 1000) {
+    mqttClient.loop();
+    delay(25);
+  }
+}
+
+void loop() {
+  if (!preventSleep) {
+    goToSleep();
+  }
+
+  if (!mqttClient.connected()) {
+    mqttClient.connect(MQTT_CLIENT_ID, MQTT_USER, MQTT_PASSWORD);
+    mqttClient.subscribe(SLEEP_TOPIC, 1);
+  }
+
+  mqttClient.loop();
+
+  if (millis() - lastBlink >= 500) {
+    lastBlink = millis();
+    digitalWrite(LED_PIN, !digitalRead(LED_PIN));
+  }
+}
